60c7a4e46f3029d113ae53efbe9ec755cd4e90c6
KinJonathan <kinialejonathan@gmail.com>
2026-06-25 11:21:51 +0300

    JavaScript getting the five Dom elements

---
+++ currency-converter/index.js
@@ -0,0 +1,8 @@
+const currencyFirstE1 = document.getElementById("currency-first");
+const worthFirstE1 = document.getElementById("worth-second");
+
+const currencySecondtE1 = document.getElementById("currency-second");
+const worthSecondtE1 = document.getElementById("worth-second");
+
+const exchangeRateE1 = document.getElementById("exchange-rate");
+
